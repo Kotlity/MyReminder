fab54dc80ca30fd797f7fc2f4a2412ae19ef5df8
Kotlity <frankierafie12@gmail.com>
2025-01-03 16:19:30 +0200

    Deleted unnecessary isAlertDialogRationaleVisible field in RemindersState

--- feature_reminders/presentation/src/main/java/com/kotlity/feature_reminders/presentation/actions/RemindersAction.kt
+++ feature_reminders/presentation/src/main/java/com/kotlity/feature_reminders/presentation/actions/RemindersAction.kt
@@ -4,7 +4,6 @@ sealed interface RemindersAction {
     data class OnReminderSelect(val position: Pair<Int, Int>, val id: Long): RemindersAction
     data class OnReminderEdit(val id: Long): RemindersAction
     data class OnReminderDelete(val id: Long): RemindersAction
-    data object OnIsAlertDialogRationaleVisibleUpdate: RemindersAction
     data object OnReminderRestore: RemindersAction
     data object OnReminderUnselect: RemindersAction
     data object OnLoadReminders: RemindersAction
